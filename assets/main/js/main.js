a09e35b95c6c948d0f3ff3c43616ea862ed24c46
Hamid Axtamov <hamidaxtamov1@gmail.com>
2023-11-18 17:35:11 +0500

    js fixed navbar

--- assets/main/js/main.js
+++ assets/main/js/main.js
@@ -1,3 +1,18 @@
 const toggleMenu = () => {
         document.body.classList.toggle("open");
       };
+
+
+let navbarText = document.querySelector('.page-title');
+
+window.addEventListener('scroll', function() {
+    // Calculate the opacity based on the scroll position
+    let opacity = 1 - window.scrollY / 150; // You can adjust the divisor for a faster or slower fade
+
+    // Ensure opacity is between 0 and 1
+    opacity = Math.min(1, Math.max(0, opacity));
+
+    // Set the opacity of the navbar text
+    navbarText.style.opacity = opacity;
+});
+
